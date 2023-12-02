8874f34b4cec5083b081d7a72827c7f9f64d4463
SX-9 <sx-91@outlook.com>
2023-12-02 01:50:14 +0000

    fix whitelist

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-process.setMaxListeners(0);
-
 const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express();
@@ -10,14 +8,13 @@ const defaultHost = process.env.DEFAULT_HOST || 'localhost';
 
 app.setMaxListeners(Infinity);
 app.set('trust proxy', true);
-
 let last = {
     port: 80,
     host: defaultHost,
     prot: 'http',
 };
 app.use('/', (req, res) => {
-    console.log(`${req.ip} ${req.method} ${req.url} ${req.headers.referer?.split('?')[1] || ''}`);
+    console.log(`> Input ${req.ip} ${req.method} ${req.url}`);
 
     const referer = new URL(req.headers.referer || 'http://localhost:80');
     const refererParams = new URLSearchParams(referer.search);
@@ -26,12 +23,15 @@ app.use('/', (req, res) => {
     const prot = req.query.prot || refererParams.get('prot') || last.prot;
     const target = `${prot}://${host}:${port}`;
     last = {port,host,prot};
-    if (whitelist_port.length > 0 && !whitelist_port.includes(port)) {
+
+    debugger
+    if (whitelist_port.length > 0 && !whitelist_port.includes(port.toString())) {
+        console.log("> Forbidden", port);
         res.status(403).send('Forbidden');
         return;
     }
-
     if (whitelist_host.length > 0 && !whitelist_host.includes(host)) {
+        console.log("> Forbidden", host);
         res.status(403).send('Forbidden');
         return;
     }
@@ -43,6 +43,7 @@ app.use('/', (req, res) => {
         ws: true,
     });
     proxy(req, res);
+    console.log("> Output", target);
 });
 
 app.listen(port, () => {
---
+++ test.sh
@@ -0,0 +1,4 @@
+export DEFAULT_HOST="192.168.1.42"
+export WHITELIST_HOST="192.168.1.42,code-laptop"
+export WHITELIST_PORT="80,8500"
+node --inspect server.js
